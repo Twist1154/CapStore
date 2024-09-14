daf28cd33a1ae88a51e589987fb96bb4e24dc2b4
ZEDAYa1 <220097429@mycput.ac.za>
2024-09-14 23:27:19 +0200

    Discount and images added

---
+++ src/main/java/za/ac/cput/service/DiscountService.java
@@ -0,0 +1,46 @@
+package za.ac.cput.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Discount;
+import za.ac.cput.repository.IDiscountRepository;
+
+import java.util.List;
+
+@Service
+public class DiscountService implements IDiscountService {
+    private final IDiscountRepository discountRepository;
+
+    @Autowired
+    public DiscountService(IDiscountRepository discountRepository){
+        this.discountRepository = discountRepository;
+    }
+
+    @Override
+    public Discount create(Discount discount) {
+        return discountRepository.save(discount);
+    }
+
+    @Override
+    public Discount read(Long id) {
+        return discountRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Discount update(Discount discount) {
+        if(discountRepository.existsById(discount.getDiscountId())){
+            return discountRepository.save(discount);
+        }
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+        this.discountRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Discount> findAll() {
+        return discountRepository.findAll();
+    }
+}
